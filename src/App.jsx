bed606fcd3805fb31ae8bf634d8e2ca210f0d28d
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-08-31 18:18:33 +0200

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from "@tanstack/react-router";
+import { Outlet,  } from "@tanstack/react-router";
 import "./App.css";
 
 function App() {
@@ -6,29 +6,17 @@ function App() {
     <>
       <header>
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/Login">Login</Link>
-            </li>
-          </ul>
+          
         </nav>
       </header>
 
       <main>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     
         <Outlet />
       </main>
 
       <footer>
-        <small>Created with ❤️ by _REPLACE_</small>
+        
       </footer>
     </>
   );
