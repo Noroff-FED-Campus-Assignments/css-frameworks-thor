ee45398e4861c5df5c90778666f6cefdfbd61771
alexvedt <alexvedt@gmail.com>
2023-08-31 17:05:08 +0200

    fixed login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Outlet, Link } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import Sidebar from "./components/Sidebar";
 import "./App.css";
 
@@ -8,31 +8,14 @@ function App() {
   return (
     <>
       <Sidebar />
-      
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link> {/* Correct the link to "login" */}
-            </li>
-          </ul>
-        </nav>
-      </header>
-
       <main>
         {/* NOTE: Outlet is where the pages are rendered */}
         <Outlet/>
       </main>
 
-      <footer>
+      {/* <footer>
         <small>Created with ❤️ by _REPLACE_</small>
-      </footer>
+      </footer> */}
     </>
   );
 }
