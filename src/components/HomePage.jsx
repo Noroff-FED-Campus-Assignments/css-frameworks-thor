6c2488dc19dec79b02ec73e40f40cf79bc23c88d
Ada Keita <mathildebrakstad@gmail.com>
2023-08-31 22:34:34 +0200

    tweaked responsiveness

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,11 +5,9 @@ import Feed from './Feed';
 function HomePage() {
   return (
     <div className="container mx-auto px-4">
-      <div className="flex flex-row justify-between items-center">
-        <h1 className='text-xl font-extrabold'>CodeConnect</h1>
-        <div className="flex justify-between items-center mb-8">
-          <SearchBar className="bg-white text-gray-800 rounded-md px-4 py-2 w-1/2" />
-        </div>
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+        <h1 className="text-xl font-extrabold mb-4 md:mb-0">CodeConnect</h1>
+        <SearchBar className="bg-white text-gray-800 rounded-md px-4 py-2 w-full md:w-1/2" />
       </div>
 
       <div className="grid gap-6">
